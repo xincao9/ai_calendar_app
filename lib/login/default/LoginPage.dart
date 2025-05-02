44780da86403ccd8484c4691e841f7b5c796ffb4
xincao9 <xincao9@gmail.com>
2025-05-02 19:58:00 +0800

    优化

---
+++ lib/login/default/LoginPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'LoginPageState.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  LoginPageState createState() => LoginPageState();
+}
